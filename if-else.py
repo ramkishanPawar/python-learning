0236b403c6a6c67dc212d8e846dd0013cb6608f1
Ramkishan <ramkishanpawar95@gmail.com>
2019-05-14 21:03:24 +0530

    added if-else script

---
+++ if-else.py
@@ -0,0 +1,11 @@
+"""
+@author: Ramkishan K Pawar
+python program to demostrate if-else.
+"""
+
+name = input("Enter Your name Here: ")
+
+if name:
+    print("Hello, " + name + "!")
+else:
+    print("Please enter valid name !")
