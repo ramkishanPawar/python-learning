4e0528cfc4c6df9e953f25605406c243811e9aa2
Ramkishan <ramkishanpawar95@gmail.com>
2019-05-26 18:49:23 +0530

    added file

---
+++ balance.py
@@ -0,0 +1,13 @@
+""" Python Program demonstrating the use of if 
+statement.
+@author: Ramkishan K Pawar
+"""
+
+phone_balance = 3
+bank_balance = 100
+
+if phone_balance < 5:
+    phone_balance += 10
+    bank_balance -= 10
+
+print("Phone Balance is {} and bank balance is {}".format(phone_balance,bank_balance))
